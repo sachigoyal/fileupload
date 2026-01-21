7464a9aba697003acb19908b02e30fde221f59b0
sachigoyal <sachigoyal27@gmail.com>
2026-01-21 14:54:39 +0530

    .

---
+++ src/lib/config.ts
@@ -0,0 +1,23 @@
+export const siteConfig = {
+    name: "FileUpload",
+    title: "FileUpload",
+    description: "Composable FileUpload component that you can use to upload files to your server",
+    origin: "https://fileupload.sachi.dev",
+    og: "https://fileupload.sachi.dev/og.png",
+    keywords: [
+      "fileupload",
+      "file upload",
+      "file upload component",
+      "file upload search",
+      "file upload search component",
+      "file upload search component",
+    ],
+    creator: {
+      name: "Sachi Goyal",
+      url: "https://sachi.dev",
+    },
+    socials: {
+      github: "https://github.com/sachigoyal/fileupload",
+      x: "https://x.com/sachigoyal27",
+    }
+  }
